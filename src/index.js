8fa08649d716b023b4d8bac26ee858ffb942810d
iserveit-llc <anshuman.chowdhary75@gmail.com>
2026-06-27 02:05:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import App from './App';
-
+//ADDED DUMMY CHANGES FOR DEPOLOYMENT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
